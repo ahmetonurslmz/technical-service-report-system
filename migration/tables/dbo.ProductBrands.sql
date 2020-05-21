cf6560974e4ae931aabbad6dac3ec168013355c6
Ahmet Onur Solmaz <ahmetonurslmz@gmail.com>
2020-05-21 22:32:32 +0300

    creates all tables and its migration sql files.

---
+++ migration/tables/dbo.ProductBrands.sql
@@ -0,0 +1,6 @@
+﻿CREATE TABLE [dbo].[ProductBrands]
+(
+	[product_brand_id]      INT   IDENTITY (1, 1) NOT NULL,
+    [product_brand_name]	NCHAR (50) NOT NULL,
+	PRIMARY KEY CLUSTERED ([product_brand_id] ASC),
+)
---
+++ migration/tables/dbo.ProductModels.sql
@@ -0,0 +1,7 @@
+﻿CREATE TABLE [dbo].[ProductModels] (
+    [product_model_id]   INT          IDENTITY (1, 1) NOT NULL,
+    [product_model_name] VARCHAR (20) NOT NULL,
+	[product_brand_id]      INT        NOT NULL,
+    PRIMARY KEY CLUSTERED ([product_model_id] ASC),
+	FOREIGN KEY ([product_brand_id]) REFERENCES [dbo].[ProductBrands] ([product_brand_id])
+);
